4d9ad69950728f418cd8631302abb83e4785ede1
Boots2291 <buteramv@gmail.com>
2017-07-24 09:35:35 -0600

    Add artists test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -4,4 +4,35 @@ require 'minitest/pride'
 require './lib/curator'
 
 class CuratorTest < Minitest::Test
+  def test_it_exists
+    curator = Curator.new
+
+    assert_instance_of Curator, curator
+  end
+
+  def test_it_holds_empty_array_of_artists
+    curator = Curator.new
+
+    assert_equal [], curator.artists
+  end
 end
+
+# > curator = Curator.new
+# # => #<Curator:0x007f98aa0617e8 ...>
+#
+# > curator.artists
+# # => []
+#
+# > curator.museums
+# # => []
+#
+# > curator.photographs
+# # => []
+#
+# > curator.add_museum({name: "MoMA"})
+#
+# > curator.museums
+# # => [#<Museum:0x007f98a91a86e8 @id=1, @name="MoMA">]
+#
+# > curator.museums.count
+# # => 1
