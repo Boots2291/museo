43e229cabaf467326f4f6520deb61eee46a1beb1
Boots2291 <buteramv@gmail.com>
2017-07-24 09:31:20 -0600

    Add working name method for photograph

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,12 +1,14 @@
 class Photograph
   attr_reader :id,
               :artist_id,
-              :museum_id
+              :museum_id,
+              :name
 
   def initialize(hash)
     @id = hash[:id]
     @artist_id = hash[:artist_id]
     @museum_id = hash[:museum_id]
+    @name = hash[:name]
   end
 
 
