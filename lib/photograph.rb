2ca70a2a19a203c1bb1fcec877dfc45cfc86fedc
Boots2291 <buteramv@gmail.com>
2017-07-24 09:32:49 -0600

    Year test is passing

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -2,13 +2,15 @@ class Photograph
   attr_reader :id,
               :artist_id,
               :museum_id,
-              :name
+              :name,
+              :year
 
   def initialize(hash)
     @id = hash[:id]
     @artist_id = hash[:artist_id]
     @museum_id = hash[:museum_id]
     @name = hash[:name]
+    @year = hash[:year]
   end
 
 
