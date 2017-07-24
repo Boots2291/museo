c27333ea7bb16838a3443a4692de955720465147
Boots2291 <buteramv@gmail.com>
2017-07-24 09:09:32 -0600

    Add second test

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -13,6 +13,15 @@ class PhotographTest < Minitest::Test
     assert_instance_of Photograph, photograph
   end
 
+  def test_it_has_an_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                                 artist_id: 4,
+                                 museum_id: 2,
+                                 year: 1954})
+
+    assert_equal 1, photograph.id
+  end
+
 end
 
 
