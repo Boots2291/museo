d6851ab83024b59e79381da54d75cb313bd93da1
Boots2291 <buteramv@gmail.com>
2017-07-24 09:26:45 -0600

    Museum_id test is passing

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,11 +1,13 @@
 class Photograph
   attr_reader :id,
-              :artist_id
+              :artist_id,
+              :museum_id
   # @id = 0
 
   def initialize(hash)
     # @id = id
     @artist_id = hash[:artist_id]
+    @museum_id = hash[:museum_id]
   end
 
 
